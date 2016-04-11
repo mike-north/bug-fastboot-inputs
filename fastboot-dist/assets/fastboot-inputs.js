4a4314b77999d9fac77d5be835f1e59b08d25665
Mike North <michael.l.north@gmail.com>
2016-04-11 15:38:03 +0200

    Fastboot inputs bug

---
+++ fastboot-dist/assets/fastboot-inputs.js
@@ -0,0 +1,435 @@
+"use strict";
+
+/* jshint ignore:start */
+
+
+
+/* jshint ignore:end */
+
+define('fastboot-inputs/app', ['exports', 'ember', 'fastboot-inputs/resolver', 'ember-load-initializers', 'fastboot-inputs/config/environment'], function (exports, _ember, _fastbootInputsResolver, _emberLoadInitializers, _fastbootInputsConfigEnvironment) {
+
+  var App = undefined;
+
+  _ember['default'].MODEL_FACTORY_INJECTIONS = true;
+
+  App = _ember['default'].Application.extend({
+    modulePrefix: _fastbootInputsConfigEnvironment['default'].modulePrefix,
+    podModulePrefix: _fastbootInputsConfigEnvironment['default'].podModulePrefix,
+    Resolver: _fastbootInputsResolver['default']
+  });
+
+  (0, _emberLoadInitializers['default'])(App, _fastbootInputsConfigEnvironment['default'].modulePrefix);
+
+  exports['default'] = App;
+});
+define('fastboot-inputs/components/app-version', ['exports', 'ember-cli-app-version/components/app-version', 'fastboot-inputs/config/environment'], function (exports, _emberCliAppVersionComponentsAppVersion, _fastbootInputsConfigEnvironment) {
+
+  var name = _fastbootInputsConfigEnvironment['default'].APP.name;
+  var version = _fastbootInputsConfigEnvironment['default'].APP.version;
+
+  exports['default'] = _emberCliAppVersionComponentsAppVersion['default'].extend({
+    version: version,
+    name: name
+  });
+});
+define('fastboot-inputs/helpers/pluralize', ['exports', 'ember-inflector/lib/helpers/pluralize'], function (exports, _emberInflectorLibHelpersPluralize) {
+  exports['default'] = _emberInflectorLibHelpersPluralize['default'];
+});
+define('fastboot-inputs/helpers/singularize', ['exports', 'ember-inflector/lib/helpers/singularize'], function (exports, _emberInflectorLibHelpersSingularize) {
+  exports['default'] = _emberInflectorLibHelpersSingularize['default'];
+});
+define('fastboot-inputs/initializers/app-version', ['exports', 'ember-cli-app-version/initializer-factory', 'fastboot-inputs/config/environment'], function (exports, _emberCliAppVersionInitializerFactory, _fastbootInputsConfigEnvironment) {
+  exports['default'] = {
+    name: 'App Version',
+    initialize: (0, _emberCliAppVersionInitializerFactory['default'])(_fastbootInputsConfigEnvironment['default'].APP.name, _fastbootInputsConfigEnvironment['default'].APP.version)
+  };
+});
+define('fastboot-inputs/initializers/container-debug-adapter', ['exports', 'ember-resolver/container-debug-adapter'], function (exports, _emberResolverContainerDebugAdapter) {
+  exports['default'] = {
+    name: 'container-debug-adapter',
+
+    initialize: function initialize() {
+      var app = arguments[1] || arguments[0];
+
+      app.register('container-debug-adapter:main', _emberResolverContainerDebugAdapter['default']);
+      app.inject('container-debug-adapter:main', 'namespace', 'application:main');
+    }
+  };
+});
+define('fastboot-inputs/initializers/data-adapter', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `data-adapter` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'data-adapter',
+    before: 'store',
+    initialize: _ember['default'].K
+  };
+});
+define('fastboot-inputs/initializers/ember-data', ['exports', 'ember-data/setup-container', 'ember-data/-private/core'], function (exports, _emberDataSetupContainer, _emberDataPrivateCore) {
+
+  /*
+  
+    This code initializes Ember-Data onto an Ember application.
+  
+    If an Ember.js developer defines a subclass of DS.Store on their application,
+    as `App.StoreService` (or via a module system that resolves to `service:store`)
+    this code will automatically instantiate it and make it available on the
+    router.
+  
+    Additionally, after an application's controllers have been injected, they will
+    each have the store made available to them.
+  
+    For example, imagine an Ember.js application with the following classes:
+  
+    App.StoreService = DS.Store.extend({
+      adapter: 'custom'
+    });
+  
+    App.PostsController = Ember.ArrayController.extend({
+      // ...
+    });
+  
+    When the application is initialized, `App.ApplicationStore` will automatically be
+    instantiated, and the instance of `App.PostsController` will have its `store`
+    property set to that instance.
+  
+    Note that this code will only be run if the `ember-application` package is
+    loaded. If Ember Data is being used in an environment other than a
+    typical application (e.g., node.js where only `ember-runtime` is available),
+    this code will be ignored.
+  */
+
+  exports['default'] = {
+    name: 'ember-data',
+    initialize: _emberDataSetupContainer['default']
+  };
+});
+define('fastboot-inputs/initializers/export-application-global', ['exports', 'ember', 'fastboot-inputs/config/environment'], function (exports, _ember, _fastbootInputsConfigEnvironment) {
+  exports.initialize = initialize;
+
+  function initialize() {
+    var application = arguments[1] || arguments[0];
+    if (_fastbootInputsConfigEnvironment['default'].exportApplicationGlobal !== false) {
+      var value = _fastbootInputsConfigEnvironment['default'].exportApplicationGlobal;
+      var globalName;
+
+      if (typeof value === 'string') {
+        globalName = value;
+      } else {
+        globalName = _ember['default'].String.classify(_fastbootInputsConfigEnvironment['default'].modulePrefix);
+      }
+
+      if (!window[globalName]) {
+        window[globalName] = application;
+
+        application.reopen({
+          willDestroy: function willDestroy() {
+            this._super.apply(this, arguments);
+            delete window[globalName];
+          }
+        });
+      }
+    }
+  }
+
+  exports['default'] = {
+    name: 'export-application-global',
+
+    initialize: initialize
+  };
+});
+define('fastboot-inputs/initializers/injectStore', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `injectStore` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'injectStore',
+    before: 'store',
+    initialize: _ember['default'].K
+  };
+});
+define('fastboot-inputs/initializers/store', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `store` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'store',
+    after: 'ember-data',
+    initialize: _ember['default'].K
+  };
+});
+define('fastboot-inputs/initializers/transforms', ['exports', 'ember'], function (exports, _ember) {
+
+  /*
+    This initializer is here to keep backwards compatibility with code depending
+    on the `transforms` initializer (before Ember Data was an addon).
+  
+    Should be removed for Ember Data 3.x
+  */
+
+  exports['default'] = {
+    name: 'transforms',
+    before: 'store',
+    initialize: _ember['default'].K
+  };
+});
+define("fastboot-inputs/instance-initializers/browser/clear-double-boot", ["exports"], function (exports) {
+  /*globals Ember*/
+
+  // When using `ember fastboot --serve-assets` the application output will
+  // already be rendered to the DOM when the actual JavaScript loads. Ember
+  // does not automatically clear its `rootElement` so this leads to the
+  // "double" applications being visible at once (only the "bottom" one is
+  // running via JS and is interactive).
+  //
+  // This removes any pre-rendered ember-view elements, so that the booting
+  // application will replace the pre-rendered output
+
+  exports["default"] = {
+    name: "clear-double-boot",
+
+    initialize: function initialize(instance) {
+      var originalDidCreateRootView = instance.didCreateRootView;
+
+      instance.didCreateRootView = function () {
+        Ember.$(instance.rootElement + ' .ember-view').remove();
+
+        originalDidCreateRootView.apply(instance, arguments);
+      };
+    }
+  };
+});
+define("fastboot-inputs/instance-initializers/ember-data", ["exports", "ember-data/-private/instance-initializers/initialize-store-service"], function (exports, _emberDataPrivateInstanceInitializersInitializeStoreService) {
+  exports["default"] = {
+    name: "ember-data",
+    initialize: _emberDataPrivateInstanceInitializersInitializeStoreService["default"]
+  };
+});
+define('fastboot-inputs/resolver', ['exports', 'ember-resolver'], function (exports, _emberResolver) {
+  exports['default'] = _emberResolver['default'];
+});
+define('fastboot-inputs/router', ['exports', 'ember', 'fastboot-inputs/config/environment'], function (exports, _ember, _fastbootInputsConfigEnvironment) {
+
+  var Router = _ember['default'].Router.extend({
+    location: _fastbootInputsConfigEnvironment['default'].locationType
+  });
+
+  Router.map(function () {});
+
+  exports['default'] = Router;
+});
+define('fastboot-inputs/services/ajax', ['exports', 'ember-ajax/services/ajax'], function (exports, _emberAjaxServicesAjax) {
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function get() {
+      return _emberAjaxServicesAjax['default'];
+    }
+  });
+});
+define('fastboot-inputs/services/fastboot', ['exports', 'ember'], function (exports, _ember) {
+
+  var alias = _ember['default'].computed.alias;
+  var computed = _ember['default'].computed;
+
+  exports['default'] = _ember['default'].Service.extend({
+    cookies: alias('_fastbootInfo.cookies'),
+    headers: alias('_fastbootInfo.headers'),
+    host: computed(function () {
+      return this._fastbootInfo.host();
+    }),
+    isFastBoot: computed(function () {
+      return typeof FastBoot !== 'undefined';
+    })
+  });
+});
+/* global FastBoot */
+define("fastboot-inputs/templates/application", ["exports"], function (exports) {
+  exports["default"] = Ember.HTMLBars.template((function () {
+    return {
+      meta: {
+        "fragmentReason": {
+          "name": "missing-wrapper",
+          "problems": ["multiple-nodes", "wrong-type"]
+        },
+        "revision": "Ember@2.4.4",
+        "loc": {
+          "source": null,
+          "start": {
+            "line": 1,
+            "column": 0
+          },
+          "end": {
+            "line": 29,
+            "column": 0
+          }
+        },
+        "moduleName": "fastboot-inputs/templates/application.hbs"
+      },
+      isEmpty: false,
+      arity: 0,
+      cachedFragment: null,
+      hasRendered: false,
+      buildFragment: function buildFragment(dom) {
+        var el0 = dom.createDocumentFragment();
+        var el1 = dom.createElement("h2");
+        dom.setAttribute(el1, "id", "title");
+        var el2 = dom.createTextNode("Welcome to Ember");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createElement("p");
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("div");
+        var el3 = dom.createTextNode("A W3C input (disabled is not present in attrs)");
+        dom.appendChild(el2, el3);
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("input");
+        dom.setAttribute(el2, "type", "text");
+        dom.setAttribute(el2, "value", "Hello Input!");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createElement("p");
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("div");
+        var el3 = dom.createTextNode("A W3C input (disabled is present in attrs)");
+        dom.appendChild(el2, el3);
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("input");
+        dom.setAttribute(el2, "type", "text");
+        dom.setAttribute(el2, "disabled", "");
+        dom.setAttribute(el2, "value", "Hello Input!");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createElement("p");
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("div");
+        var el3 = dom.createTextNode("An Ember input (disabled is not specified)");
+        dom.appendChild(el2, el3);
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createComment("");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createElement("p");
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("div");
+        var el3 = dom.createTextNode("An Ember input (disabled=true)");
+        dom.appendChild(el2, el3);
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createComment("");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createElement("p");
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createElement("div");
+        var el3 = dom.createTextNode("An Ember input (disabled=false)");
+        dom.appendChild(el2, el3);
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n  ");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createComment("");
+        dom.appendChild(el1, el2);
+        var el2 = dom.createTextNode("\n");
+        dom.appendChild(el1, el2);
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n\n");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createComment("");
+        dom.appendChild(el0, el1);
+        var el1 = dom.createTextNode("\n");
+        dom.appendChild(el0, el1);
+        return el0;
+      },
+      buildRenderNodes: function buildRenderNodes(dom, fragment, contextualElement) {
+        var morphs = new Array(4);
+        morphs[0] = dom.createMorphAt(dom.childAt(fragment, [6]), 3, 3);
+        morphs[1] = dom.createMorphAt(dom.childAt(fragment, [8]), 3, 3);
+        morphs[2] = dom.createMorphAt(dom.childAt(fragment, [10]), 3, 3);
+        morphs[3] = dom.createMorphAt(fragment, 12, 12, contextualElement);
+        return morphs;
+      },
+      statements: [["inline", "input", [], ["type", "text", "value", "Hello Input!"], ["loc", [null, [15, 2], [15, 44]]]], ["inline", "input", [], ["type", "text", "disabled", true, "value", "Hello Input!"], ["loc", [null, [20, 2], [20, 58]]]], ["inline", "input", [], ["type", "text", "disabled", false, "value", "Hello Input!"], ["loc", [null, [25, 2], [25, 59]]]], ["content", "outlet", ["loc", [null, [28, 0], [28, 10]]]]],
+      locals: [],
+      templates: []
+    };
+  })());
+});
+/* jshint ignore:start */
+
+
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+define('fastboot-inputs/config/environment', ['ember'], function(Ember) {
+  return { 'default': {"modulePrefix":"fastboot-inputs","environment":"development","baseURL":"/","locationType":"auto","EmberENV":{"FEATURES":{}},"APP":{"name":"fastboot-inputs","version":"0.0.0+f285e775"},"exportApplicationGlobal":true}};
+});
+
+/* jshint ignore:end */
+
+/* jshint ignore:start */
+
+if (!runningTests) {
+  require("fastboot-inputs/app")["default"].create({"name":"fastboot-inputs","version":"0.0.0+f285e775"});
+}
+
+define('~fastboot/app-factory', ['fastboot-inputs/app', 'fastboot-inputs/config/environment'], function(App, config) {
+  App = App['default'];
+  config = config['default'];
+
+  return {
+    'default': function() {
+      return App.create(config.APP);
+    }
+  };
+});
+
+
+/* jshint ignore:end */
+//# sourceMappingURL=fastboot-inputs.map
